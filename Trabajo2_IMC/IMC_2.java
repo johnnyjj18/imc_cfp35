68f5ba9a92fc5428a873c09110044e0e07fe1c3f
Irene Rendon <irenerendon18@gmail.com>
2023-05-12 12:17:46 -0300

    Actualizacion TP 2

--- Trabajo2_IMC/IMC_2.java
+++ Trabajo2_IMC/IMC_2.java
@@ -4,14 +4,16 @@ import java.text.DecimalFormat;
 public class IMC_2 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         System.out.println("***************************************************************");
         System.out.println("************ CALCULADORA - INDICE DE MASA CORPORAL ************");
         System.out.println("***************************************************************");
-        System.out.println("Ingrese su peso en kilogramos: ");
-        double peso = sc.nextDouble();
-        System.out.println("Ingrese su altura en metros: ");
-        double altura = sc.nextDouble();
+
+        double peso = Double.parseDouble(args[0]);
+        double altura = Double.parseDouble(args[1]);
+        
+        System.out.println("Ingrese su peso en kilogramos: "+ peso);
+        System.out.println("Ingrese su altura en metros: "+ altura);
 
         double imc = calcularMasaCorporal(peso, altura);
 
@@ -30,17 +32,17 @@ public class IMC_2 {
     public static String obtenerEstado(double imc) {
         if (imc < 15) {
             return "Delgadez muy severa";
-        } else if (imc >= 15 && imc <= 15.9) {
+        } else if (imc >= 15 && imc <= 15.99) {
             return "Delgadez severa";
-        } else if (imc >= 16 && imc <= 18.4) {
+        } else if (imc >= 16 && imc <= 18.49) {
             return "Delgadez";
-        } else if (imc >= 18.5 && imc <= 24.9) {
+        } else if (imc >= 18.5 && imc <= 24.99) {
             return "Correcto";
-        } else if (imc >= 25 && imc <= 29.9) {
+        } else if (imc >= 25 && imc <= 29.99) {
            return "Sobrepeso";
-        } else if (imc >= 30 && imc <= 34.9) {
+        } else if (imc >= 30 && imc <= 34.99) {
             return "Obesidad Moderada";
-        } else if (imc >= 35 && imc <= 39.9) {
+        } else if (imc >= 35 && imc <= 39.99) {
            return "Obesidad Severa";
         } else {
             return "Obesidad Mórbida";
